2d6c2c1362dd4cf87efc3a428bc984f5c6844d1d
cuongbuoi <cuongyo19997@gmail.com>
2018-04-19 19:47:04 +0700

    fix loi MTQ

--- ViewApp/header.php
+++ ViewApp/header.php
@@ -4,18 +4,6 @@
 		
 ?>
 
- 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Website Truong bo mon</title>
-<link href="ViewAdmin/style.css" rel="stylesheet" type="text/css" />
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-<link href="css/style.css" rel="stylesheet" type="text/css" />
-</head>
-
-<body>
 
   
   <div class="header">
@@ -59,14 +47,5 @@
 <li><a href="index.php?f=QuanTri_ThongTinCanBo">Đổi mật khẩu</a></li>
 
 </div>
-  
-  
-  
-  
-  
-  
- 
-</div><!--end wrapper-->
 
-</body>
-</html>
+  
